908a6db5c09f817f840e7912336efff2b41e3b0b
Anastasiia <nastia.kvas.89@gmail.com>
2017-09-14 19:30:56 +0300

    again changes2

--- src/main/java/RepairService.java
+++ src/main/java/RepairService.java
@@ -12,7 +12,8 @@ public class RepairService {
         if (vehicle.damage > 0) {
             health = vehicle.damage - vehicle.damage;
             System.out.println("Service successfully repaired the " + vehicle);
-            System.out.println(health);
+            System.out.println("Health now is " + health);
+            System.out.println();
         } else{
             if (health == 0){
                 System.out.println("nothing");
